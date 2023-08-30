e8b3ca294a9561d8c94100e693761040b1884584
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-08-31 00:49:49 +0300

    hash dinleme ve route function olusuturuldu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,34 @@
-const subFormWrapper = document.querySelector('#subFormWrapper');
-const createSub = document.querySelector('#createSub');
+addEventListener('hashchange', handleRoute);
 
-createSub.addEventListener('click', () => {
-  subFormWrapper.classList.toggle('hidden');
-});
+const routes = {
+  '/': {
+    title: 'Anasayfa',
+    pages: 'anasayfa',
+  },
+  '/signup': {
+    title: 'Üye ol',
+    pages: 'signup',
+  },
+  '/404': {
+    title: 'Sayfa Bulunamadı',
+    pages: '404',
+  },
+};
+
+const rootEl = document.querySelector('.root');
+
+async function handleRoute() {
+  let url = location.hash.substring(1);
+
+  if (url.length < 1) {
+    url = '/';
+  }
+
+  const route = routes[url] || routes['/404'];
+
+  document.title = route.title;
+
+  rootEl.innerHTML = await fetch(`/pages/${route.pages}.html`).then(r => r.text());
+}
+
+handleRoute();
